dba9aa8dec1faadd5089866107cc14b3f4b8a6cb
luankefei <luankefei@gmail.com>
2015-12-07 14:37:53 +0800

    header underbar

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -15,7 +15,15 @@ class Header extends React.Component {
     return (
       <header>
         <h1>{this.props.title}</h1>
-        <nav>dropdown here.</nav>
+        <div className='clearfix'>
+          <div className='business'>
+            <img src="http://wx.qlogo.cn/mmopen/IbQhd2Y9oKc2zOTLXD4TZqVVh3BAHWLuGed8lcGJYgVsDaXTLOJp1IDWEZtEyUzEVLSLqGdDGEkdekGIKXCMHQ/0" alt="星巴克" />
+            <dl>
+              <dt>星巴克代购</dt>
+              <dd>24小时营业 9元外送费</dd>
+            </dl>
+          </div>
+        </div>
       </header>
     );
   }
